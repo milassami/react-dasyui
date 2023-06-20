d106b4696b37d80aa444b5e8a0f8814d606b7594
milassami <91599903+milassami@users.noreply.github.com>
2023-06-20 09:49:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-dasyui
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-webpack-3dotha)
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+import Navbar from './components/Navbar.js';
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+export default App;
