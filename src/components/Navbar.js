fd3fe0dfa160169a31338c99ddc9d96c337c9d14
milassami <91599903+milassami@users.noreply.github.com>
2023-06-21 11:09:38 +0300

    1 file modified, 1 added, 1 deleted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav class="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center">
-          {/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo"/> */}
+          <img src="../assets/class.png" class="h-8 mr-3" alt=""/>
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             
           </span>
