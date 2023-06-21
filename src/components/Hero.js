af1154fedfcdaf17f1267cc8effc4757f38d19ea
milassami <91599903+milassami@users.noreply.github.com>
2023-06-21 13:32:07 +0300

    3 files modified

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,6 +17,7 @@ const Hero = () => {
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
             <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"/>
+            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"/>
         </div>                
     </div>
 </section>
